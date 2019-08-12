84090170579d4fdd6e9f478f21e03b86bc62339b
JordenHampton <platinumgamer98@gmail.com>
2019-08-12 10:27:26 +0100

    Added very basic SASS.

---
+++ about-us.php
@@ -0,0 +1,22 @@
+<?php include("inc/header.php")?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+    <link rel="stylesheet" href="css/about-style.css">
+</head>
+<body>
+    <div id="about-container">
+        <h1 class="title">About Triac</h1>
+    </div>
+    
+</body>
+</html>
+
+
+
+<?php include("inc/footer.php")?>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php include("inc/header.php")?>
-    
+    <a href="<?php
+        echo "about-us.php"
+    ?>">about us</a>
 <?php include("inc/footer.php")?>
