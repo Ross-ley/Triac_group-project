87dc2411134e1576a5e7b8aecb53f29d380fd547
JordenHampton <platinumgamer98@gmail.com>
2019-08-12 16:16:49 +0100

    Updated about us page
    Finished mobile and small device page. More work for desktop.

--- about-us.php
+++ about-us.php
@@ -1,22 +1,36 @@
 <?php include("inc/header.php")?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About Us</title>
-    <link rel="stylesheet" href="css/about-style.css">
-</head>
-<body>
     <div id="about-container">
-        <h1 class="title">About Triac</h1>
+        <div class="heading">
+            <div class="seperator">.</div>
+            <h1 class="title">About Triac</h1>
+            </div>
+            <div class="about-text">
+                <p class="about-intro intro-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et odio pellentesque diam volutpat commodo sed egestas egestas. Nec tincidunt praesent semper feugiat. Pharetra sit amet aliquam id diam maecenas ultricies mi.</p>
+                <p class="about-intro">Adipiscing at in tellus integer feugiat scelerisque. Sollicitudin tempor id eu nisl nunc. A scelerisque purus semper eget duis at tellus at. Scelerisque eleifend donec pretium vulputate sapien. Pharetra vel turpis nunc eget lorem dolor sed viverra. Fringilla urna porttitor rhoncus dolor purus. Auctor augue mauris augue neque.</p>
+            </div>
+        
+            <ul class="services">
+                <div class="ppm serv-cont">
+                    <h2>Planned Preventative Maintenance</h2>
+                    <div class="serv-back"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>    
+                </div>
+                <div class="inv-repair serv-gap serv-cont">
+                    <h2>Inverter Repairs</h2>
+                    <div class="serv-back"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>    
+                </div>
+                <div class="motor-repairs serv-gap serv-cont">
+                    <h2>Motor Repairs</h2>
+                    <div class="serv-back"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>    
+                </div>
+                
+            </ul>
+        
+        <div class="about-text">
+            <p class="about-intro intro-2">In egestas erat imperdiet sed. Placerat in egestas erat imperdiet sed euismod nisi porta. Lorem dolor sed viverra ipsum nunc aliquet. Commodo quis imperdiet massa tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum nisi quis. Nec ultrices dui sapien eget mi. Scelerisque in dictum non consectetur. At volutpat diam ut venenatis tellus.</p>
+            <p class="about-intro">Etiam sit amet nisl purus in mollis nunc sed id. Egestas congue quisque egestas diam in arcu cursus euismod. Et netus et malesuada fames ac turpis egestas. Auctor elit sed vulputate mi sit amet. Sit amet porttitor eget dolor morbi non arcu risus. Pretium fusce id velit ut tortor pretium viverra suspendisse.</p>
+            <p class="about-intro">Integer quis auctor elit sed. Elit at imperdiet dui accumsan sit amet nulla. Auctor eu augue ut lectus. Arcu risus quis varius quam quisque id. Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque.</p>
+        </div>
     </div>
-    
-</body>
-</html>
-
-
 
 <?php include("inc/footer.php")?>
