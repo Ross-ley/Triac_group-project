417b1654ba48a6ec0e1e1bbbf93763d0e391f874
Jacobv94 <51744057+Jacobv94@users.noreply.github.com>
2019-08-15 10:25:24 +0100

    Footer
    Completed most of the footer the text needs aligning image needs fixing to match color of background.

--- inc/footer.php
+++ inc/footer.php
@@ -1,2 +1,92 @@
+<?php  
+    include('header.php');
+?>
+
+
+<div class="pink"></div>
+<div class="footer">
+    <div class="links container"> 
+        <div class="row">
+            <div class="col-sm-3" id="nav">
+                <strong>Navigation</strong>
+                <br>
+                <br>
+                <ul>
+                    <li>About Us</li>
+                    <li>Services</li>
+                    <li>Inverters</li>
+                    <li>Motors</li>
+                    <li>Case Studies</li>
+                    <li>Energy Audit</li>
+                    <li>Contact Us</li>
+                </ul>
+                
+            </div>
+
+            <div class="col-sm-3" id="services">
+                <strong>Services</strong>
+                <br>
+                <br>
+                <ul>
+                    <li>Electric Motor Maintenance</li>
+                    <li>Pump Maintenance</li>
+                    <li>Inverter Maintenace</li>
+                    <li>Preventative Planned Maintenance</li>
+                    <li>Mechanical Services</li>
+                    <li>Triac on-site Service</li>
+                    <li>Ambliance Inverters</li>
+                </ul>
+            </div>
+
+            <div class="col-sm-3" id="legal" >
+                <strong>Legal</strong>
+                <br>
+                <br>
+                <ul>
+                    <li>Terms & Conditions</li>
+                    <li>Privacy</li>
+                    <li>Cookies</li>
+                </ul>
+            </div>
+
+            <div class="col-sm-3" id="info">
+                <img id="triac" src="img/triac.png">
+                <br>
+                <p><strong>Tel: 01502 531555</strong></p>
+                <p><strong>Email: info@triac-services.ltd.uk</strong></p>
+                <p>Unit 45, Colville Works.<br>Colville Road. Lowestoft, Suffolk</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="copyright"> 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3" id="ltd">Copyright &copy; Triac Services Ltd</div>
+            <div class="col-sm-5">
+                <img class="img-fluid" id="banner" src="img/banner.png">
+
+                 
+            </div>
+            <div class="col-sm-1">
+                <a class="fa fa-facebook"></a>
+                <a class="fa fa-linkedin"></a>
+            </div>
+               <div class="col-sm-3 design">Website Design By
+                   <span id="netmatters">Netmatters</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+    
+
+
+
+
+
+
+
 </body>
 </html>
